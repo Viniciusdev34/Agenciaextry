f2167d57e9308c43466f6dabe68af7901d160db2
Viniciusdev34 <160533398+Viniciusdev34@users.noreply.github.com>
2025-03-11 07:32:17 -0300

    commit inicial

---
+++ sites/script.js
@@ -0,0 +1,18 @@
+
+  let menuBtns = document.querySelectorAll(".arrow"); // Seleciona todos os botões
+let menus = document.querySelectorAll(".ul"); // Seleciona todas as ULs
+
+menuBtns.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+        let menu = menus[index]; // Pega o menu correspondente ao botão clicado
+
+        if (menu.classList.contains("open")) {
+            menu.classList.remove("open");
+        } else {
+            // Fecha todos antes de abrir o novo
+            menus.forEach(m => m.classList.remove("open"));
+            menu.classList.add("open");
+        }
+    });
+});
+
